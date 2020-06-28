427118072d85738582946b7733833ebaea061d29
Alejo Ortiz <davidalejandro64@gmail.com>
2020-06-28 11:29:50 -0500

    first commit

---
+++ models/bicicleta.js
@@ -0,0 +1,40 @@
+const Bicicleta = function(id, color, modelo, ubicacion) {
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+Bicicleta.prototype.toString = () => {
+    return `id: ${id} | color: ${color}`;
+}
+
+Bicicleta.allBicis = [];
+Bicicleta.add = (aBici) => {
+    Bicicleta.allBicis.push(aBici);
+}
+
+Bicicleta.findById = (aBiciId) => {
+    let aBici = Bicicleta.allBicis.find(x => x.id == aBiciId);
+    if (aBici)
+        return aBici
+    else
+        throw new Error(`No existe una bicicleta con el id ${aBiciId}`);
+}
+
+Bicicleta.removeById = (aBiciId) => {
+    for(let i = 0; i < Bicicleta.allBicis.length; i++) {
+        if (Bicicleta.allBicis[i].id == aBiciId) {
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }
+    }
+}
+
+var a = new Bicicleta(1, 'rojo', 'urbana', [3.43722, -76.5325]);
+var b = new Bicicleta(2, 'blanca', 'urbana', [3.427033, -76.531690]);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+module.exports = Bicicleta;
---
+++ public/js/map.js
@@ -0,0 +1,9 @@
+var map = L.map('main_map').setView([3.43722, -76.5225], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy: <a href:"https://www.openstreetmap.org/copyright">OpenStretMap</a> contributors'
+}).addTo(map);
+
+var marker1 = L.marker([3.43722, -76.5325]).addTo(map);
+var marker = L.marker([3.427033, -76.531690]).addTo(map);
+//marker.bindPopup("<b>Hola Mama</b><br>Ya llegue a casa.").openPopup();
