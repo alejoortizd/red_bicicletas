64b6f19fa580aace141772fe1b35e765da0b6924
Alejo Ortiz <davidalejandro64@gmail.com>
2020-06-28 12:26:35 -0500

    create an API of Bicicletas

--- public/js/map.js
+++ public/js/map.js
@@ -4,6 +4,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy: <a href:"https://www.openstreetmap.org/copyright">OpenStretMap</a> contributors'
 }).addTo(map);
 
-var marker1 = L.marker([3.43722, -76.5325]).addTo(map);
-var marker = L.marker([3.427033, -76.531690]).addTo(map);
+// var marker1 = L.marker([3.43722, -76.5325]).addTo(map);
+// var marker = L.marker([3.427033, -76.531690]).addTo(map);
 //marker.bindPopup("<b>Hola Mama</b><br>Ya llegue a casa.").openPopup();
+
+//llamar a ajx
+
+$.ajax({
+    dataType: 'json',
+    url: "api/bicicletas",
+    success: result => {
+        console.log(result);
+        result.bicicletas.forEach(bici => {
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        })
+    }
+})
